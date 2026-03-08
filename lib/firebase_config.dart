9d914b61cc3fc6a5568abec888210297eae54007
CarJoshEly <155939097+CarJoshEly@users.noreply.github.com>
2026-03-07 23:05:36 -0600

    first commit

--- lib/firebase_config.dart
+++ lib/firebase_config.dart
@@ -1,12 +1,10 @@
-
 import 'package:firebase_core/firebase_core.dart';
 
-
 const firebaseConfig = FirebaseOptions(
-  apiKey: "AIzaSyBKYT5qq32YM8DuOp_A6z3auzdgBFJGNxY",
-  authDomain: "control-habitos.firebaseapp.com",
-  projectId: "control-habitos",
-  storageBucket: "control-habitos.firebasestorage.app",
-  messagingSenderId: "1058689194132",
-  appId: "1:1058689194132:web:f6a9d9d1a4edfe0ec77c81"
+  apiKey: "AIzaSyDnKOYPwT-GXGHx6hjX-d2_WOO8GoZv4uA",
+  authDomain: "control-habitos1.firebaseapp.com",
+  projectId: "control-habitos1",
+  storageBucket: "control-habitos1.firebasestorage.app",
+  messagingSenderId: "283327600463",
+  appId: "1:283327600463:android:7f4635ce1b75614f0a573a"
 );
